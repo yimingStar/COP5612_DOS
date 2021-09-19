e7f7d04365067997333e09b1e65ea98ee2a204c0
YiMing <ericdreamwithvic@gmail.com>
2021-09-19 16:58:12 -0400

    removeing redundant config

--- projects/Project1/proj1.fsx
+++ projects/Project1/proj1.fsx
@@ -1,11 +1,11 @@
 
+
 #time "on"
 #load "packages.fsx"
 
 open System
 open System.Security.Cryptography
 open Akka.Actor
-open Akka.Configuration
 open Akka.FSharp
 
 // @brief Program solving square sequence problem
@@ -14,28 +14,6 @@ open Akka.FSharp
 
 // Method - Starting iterate through 1 ~ K as starting points, each sequence will be size of N.
 // Time theta(K*N)  
-let config =
-    Configuration.parse
-        @"akka {
-            log-config-on-start : on
-            stdout-loglevel : DEBUG
-            loglevel : ERROR
-            actor {
-                provider = ""Akka.Remote.RemoteActorRefProvider, Akka.Remote""
-                debug : {
-                    receive : on
-                    autoreceive : on
-                    lifecycle : on
-                    event-stream : on
-                    unhandled : on
-                }
-                remote.helios.tcp {
-                    hostname = localhost
-                    port = 5566
-                }
-            }
-        }"
-
 type ArgvInputs = {
     LeadZeros: int
     NumberOfActors: int
@@ -70,7 +48,7 @@ let lang = "F#"
 
 printfn "Start program of %s with %s" startMsg lang
 
-let system = ActorSystem.Create("proj1Server", config)
+let system = ActorSystem.Create("proj1Server")
 let mutable bitCoinStr = null 
 let argv = fsi.CommandLineArgs
 printfn "input arguments: %A" (argv) 
@@ -129,14 +107,20 @@ let mainActions (mailbox: Actor<obj>) msg =
     | :? ArgvInputs as param ->
         // create actor
         for i = 1 to param.NumberOfActors do
-        let name = "mine-actor-" + Convert.ToString(i)
-        let mineActor = spawn system name (actorOf2 CoinMining)
-        let minerInput: MiningInputs = {
-            LeadZeros = param.LeadZeros
-            Prefix = param.Prefix
-            ActorName = name
-        }
-        mineActor <! minerInput
+            let name = "mine-actor-" + Convert.ToString(i)
+            let mineActor = spawn system name (actorOf2 CoinMining)
+            let minerInput: MiningInputs = {
+                LeadZeros = param.LeadZeros
+                Prefix = param.Prefix
+                ActorName = name
+            }
+
+            // let mineRemoteActor =
+            //     spawne system "remotr-actor-" (actorOf2 CoinMining) 
+            //     [SpawnOption.Deploy (Deploy(RemoteScope(Address.Parse "akka.tcp://remote-system@127.0.0.1:9000/")))]
+
+            mineActor <! minerInput
+
     | :? BitCoin as param ->
         // found bit coin
         if isNull bitCoinStr then

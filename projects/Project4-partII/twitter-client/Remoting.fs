c996fba962052a8d37cce893a3a348ea77e377b7
Yi-Ming Chang <69064626+yimingStar@users.noreply.github.com>
2021-12-13 16:15:48 -0500

    using client template

---
+++ projects/Project4-partII/twitter-client/Remoting.fs
@@ -0,0 +1,12 @@
+﻿namespace twitter_client
+
+open WebSharper
+
+module Server =
+
+    [<Rpc>]
+    let DoSomething input =
+        let R (s: string) = System.String(Array.rev(s.ToCharArray()))
+        async {
+            return R input
+        }
---
+++ projects/Project4-partII/twitter-client/wwwroot/Scripts/WebSharper/twitter-client.js
@@ -0,0 +1,69 @@
+(function(Global)
+{
+ "use strict";
+ var twitter_client,Client,_twitterclient_Templates,WebSharper,Concurrency,Remoting,AjaxRemotingProvider,UI,Var$1,Templating,Runtime,Server,ProviderBuilder,Handler,TemplateInstance,Client$1,Templates;
+ twitter_client=Global.twitter_client=Global.twitter_client||{};
+ Client=twitter_client.Client=twitter_client.Client||{};
+ _twitterclient_Templates=Global["twitter-client_Templates"]=Global["twitter-client_Templates"]||{};
+ WebSharper=Global.WebSharper;
+ Concurrency=WebSharper&&WebSharper.Concurrency;
+ Remoting=WebSharper&&WebSharper.Remoting;
+ AjaxRemotingProvider=Remoting&&Remoting.AjaxRemotingProvider;
+ UI=WebSharper&&WebSharper.UI;
+ Var$1=UI&&UI.Var$1;
+ Templating=UI&&UI.Templating;
+ Runtime=Templating&&Templating.Runtime;
+ Server=Runtime&&Runtime.Server;
+ ProviderBuilder=Server&&Server.ProviderBuilder;
+ Handler=Server&&Server.Handler;
+ TemplateInstance=Server&&Server.TemplateInstance;
+ Client$1=UI&&UI.Client;
+ Templates=Client$1&&Client$1.Templates;
+ Client.Main$19$20=function(rvReversed)
+ {
+  return function(e)
+  {
+   var b;
+   Concurrency.StartImmediate((b=null,Concurrency.Delay(function()
+   {
+    return Concurrency.Bind((new AjaxRemotingProvider.New()).Async("twitter-client:twitter_client.Server.DoSomething:-1794501434",[e.Vars.Hole("texttoreverse").$1.Get()]),function(a)
+    {
+     rvReversed.Set(a);
+     return Concurrency.Zero();
+    });
+   })),null);
+  };
+ };
+ Client.Main=function()
+ {
+  var rvReversed,b,R,_this,t,p,i;
+  rvReversed=Var$1.Create$1("");
+  return(b=(R=rvReversed.get_View(),(_this=(t=new ProviderBuilder.New$1(),(t.h.push(Handler.EventQ2(t.k,"onsend",function()
+  {
+   return t.i;
+  },function(e)
+  {
+   var b$1;
+   Concurrency.StartImmediate((b$1=null,Concurrency.Delay(function()
+   {
+    return Concurrency.Bind((new AjaxRemotingProvider.New()).Async("twitter-client:twitter_client.Server.DoSomething:-1794501434",[e.Vars.Hole("texttoreverse").$1.Get()]),function(a)
+    {
+     rvReversed.Set(a);
+     return Concurrency.Zero();
+    });
+   })),null);
+  })),t)),(_this.h.push({
+   $:2,
+   $0:"reversed",
+   $1:R
+  }),_this))),(p=Handler.CompleteHoles(b.k,b.h,[["texttoreverse",0]]),(i=new TemplateInstance.New(p[1],_twitterclient_Templates.mainform(p[0])),b.i=i,i))).get_Doc();
+ };
+ _twitterclient_Templates.mainform=function(h)
+ {
+  Templates.LoadLocalTemplates("main");
+  return h?Templates.NamedTemplate("main",{
+   $:1,
+   $0:"mainform"
+  },h):void 0;
+ };
+}(self));
